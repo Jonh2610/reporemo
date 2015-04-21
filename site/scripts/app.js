a0a1d636390f1b59498538f83f55062b228a9dbd
Jonh <angel.archundia2610@gmail.com>
2015-04-21 12:51:51 -0500

    04. Aregar nuevos Posts staticos

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -5,12 +5,18 @@ modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
+		//Modelo
 		$scope.posts = 
-		["Posts 1 Hola",
-		"Posts 2",
-		"Posts 3",
-		"Posts 4",
-		"Posts 5",
-		"Posts 6"];
+		[{title: "Post 1", upvotes:5},
+		 {title: "Post 2", upvotes:15},
+		 {title: "Post 3", upvotes:7},
+		 {title: "Post 4", upvotes:9},
+		 {title: "Post 5", upvotes:3},
+		 {title: "Post 6", upvotes:5}];
 
+		 //Metodo del controlador
+		 $scope.addPost = function(){;
+		     $scope.posts.push({title:"Nuevo Post", upvotes: 0});
+
+};
 	}]);

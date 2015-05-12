19c6d172115f7f9658fd642b58584fc50c7c0dd3
Jonh26 <angel.archundia2610@gmail.com>
2015-05-12 12:29:45 -0500

    10. Se agregan rutas en el frontend

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,73 +1,80 @@
 console.log ("#Jonh: Cargo app.js");
-var modulo1=
-	angular.module("reeditgam",[]);
+//Inyectando el modulo de ui-router
+//Como parametro de arreglo de objetos
+//Del modulo
 
-//Creando un servicio de tipo factory
-modulo1.factory('posts',[function(){
-	//Cuerpo del factory llamado posts
-	var o = {
-		posts : [
-			{
+var modulo1 = 
+	angular.module("reeditgam",['ui.router']);  // INYECTANDO EL MODULO DEL UI.ROUTER COMO PARAMETRO DEL ARREGLO DE OBJETOS DEL MODULO.
+	// CONFIGURANDO LAS RUTAS.
+	// RECIVE UN ARREGLO DE ELEMENTOS.
+	modulo1.config(['$stateProvider',
+		'$urlRouterProvider',
+		function($stateProvider,$urlRouterProvider){
+			//INICIANDO RUTINA DE CONFIGURACION.
+			$stateProvider.state('home',{
+				//DEFINIENDO ESTADO COMO UN OBJETO .
+				url:"/home",// URL QUE DEFINE EL ESTADO.
+				//PLANTILLA BASE PARA EL ESTADO.
+				templateUrl: "/home.html",
+				controller:'mainCtrl'
+			});
+			// URL POR DEFECTO 
+			$urlRouterProvider.otherwise('home');
+		}]);
+	// CREANDO UN SERVICIO DEL TIPO FACTORY
+    modulo1.factory('posts',[function(){
+    	//CUERPO DEL FACTORY LLAMADO POST
+    	var o={
+    		posts : [
+    		{title: "post 1", upvotes: 15,
+    		comments: [
+                  {author: "Karina", body:"Esto esta geneal.", upvotes:3},
+                  {author: "Gamaliel", body:"Esto es basura.",upvotes:0}]
+    		    },
+    		    {title: "post 2", upvotes: 25,
+    		comments: [
+                  {author: "Coco", body:"Esto es bonito.", upvotes:5},
+                  {author: "Cristian", body:"Esto es aburrido.",upvotes:1}]
+    		    },
+    		    {title: "post 3", upvotes: 35,
+    		comments: [
+                  {author: "Angel", body:"Esto esta padrisimo.", upvotes:4},
+                  {author: "Daniel", body:"Esto no me gusta.",upvotes:3}]
+    		    }
+    		]
+    	};
+    	//RETORNANDO OBJETO DE DATOS PERSISTENTES.
+    	return o;
+    }]);
 
-				title: "post 1", upvotes: 4,
-				comments: [
-					{author: "Karina", body:"Esto esta de pelos.",
-					upvotes:3},
-					{author: "Gamaliel", body:"Esto es basura.",
-					upvotes:0}]
-			},
-
-			{
-
-				title: "post 2", upvotes: 15,
-				comments: [
-					{author: "Coco", body:"Esto es asombroso.",
-					upvotes:5},
-					{author: "Cristian", body:"Esto esta aburrido.",
-					upvotes:1}]
-
-				}
-		]
-	};
-	//retornando objeto de datos persistentes
-	return o;
-}]);
-
-//creando controlador ection
-//Dependcy inj
+	//CREANDO CONTROLADOR
+	//DEPENDENCY INJECTION
 modulo1.controller("mainCtrl",[
-	'$scope','posts', //Inyectando factory posts
-	function($scope, posts){
+	'$scope','posts', //INYECTANDO FACTORY POST
+	function($scope,posts){
 		$scope.test = "Hola Angular";
+		//MODELO AL CUAL SE LE ASIGNA EL RESULTADO DEL FACTORY
+		$scope.posts = posts.posts;
 
-		//Modelo al cual se le asigna
-		//el resultado del factory
+		//METODO DEL CONTROLADOR
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === "")
+			{
+				alert("NO se permite postear titulos vacios")
+				return;
+			}
+			$scope.posts.push(
+				{
+					title: $scope.title, 
+					link:  $scope.link,
+				upvotes: 0});
+			//twp.way data binding
+			$scope.title="";
+			$scope.link="";
 
-		$scope.posts = posts.posts
-
-		 //Metodo del controlador
-		 $scope.addPost = function(){
-             if (!$scope.title || $scope.title === "") {
-
-             	alert("No se permite agregar posts vacios")
-             	return;
-             }
-
-		     $scope.posts.push(
-		     	{title: $scope.title,
-		     	link: $scope.link,
-		     	upvotes: 0
-		     });
-
-		     $scope.title="";
-		     $scope.link="";
-
-};
-       //metodo que incrementa el voto 
-       //de un posts en una unidad 
-       $scope.incrementUpvotes = function (post){
-
-       	post.upvotes +=1;
-       };
-
+		};
+		// metodo que incrementa el voto de un post en una unidad
+		$scope.incrementUpvotes=function(post){
+			post.upvotes +=1;
+		};
 	}]);
